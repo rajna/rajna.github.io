37f135b3fbaf3700726e0e2a0d4e2576446cbc1d
Teach.Wu <teach.wu@zhan.com>
2016-09-14 13:51:50 +0800

    fly

---
+++ article/fly/circle.js
@@ -0,0 +1,125 @@
+function Bullet(direction) {
+	THREE.Mesh.call(this,
+		new THREE.SphereGeometry(Bullet.RADIUS, 6, 6),
+		new THREE.MeshBasicMaterial({ color: 0xe4e4e4 })
+	);
+
+
+	this.direction = direction;
+	this.scale.x=0.75;
+	this.scale.z=0.75;
+	this.speed = 300;
+	this.damage = 20;
+
+	this.wanderAngle=30;
+
+	this.ANGLE_CHANGE=5;
+
+	this.drawline=function(from,to){
+		var geometry = new THREE.Geometry();
+        var material = new THREE.LineBasicMaterial( { vertexColors: true } );
+        var color1 = new THREE.Color( 0xe4e4e4 ), color2 = new THREE.Color( 0xe4e4e4 );
+
+        // 线的材质可以由2点的颜色决定
+        var p1 = from;
+        var p2 = to;
+        geometry.vertices.push(p1);
+        geometry.vertices.push(p2);
+        geometry.colors.push( color1, color2 );
+
+        var line = new THREE.Line( geometry, material, THREE.LinePieces );
+
+        scene.add(line);
+	}
+	this.wander=function(){
+		var self=this;
+
+		//设置随机角度
+		function setAngle(displacement,wanderAngle){
+			var length=displacement.length();
+			displacement.setX(Math.cos(self.wanderAngle)*length);
+			displacement.setZ(Math.sin(self.wanderAngle)*length);
+		}
+		//拷贝当前方向单位向量,大小扩展为圆的长度
+		var circleCenter=new THREE.Vector3();
+
+		circleCenter=circleCenter.copy(this.direction);
+
+		circleCenter.multiplyScalar(Bullet.CIRCLE_DISTANCE);
+
+		//计算位移矢量
+		var displacement =new THREE.Vector3(0,0,-1);
+
+		displacement.multiplyScalar(Bullet.CIRCLE_RADIUS);
+		
+		setAngle(displacement,this.wanderAngle);
+
+		this.wanderAngle+=Math.random()*this.ANGLE_CHANGE - this.ANGLE_CHANGE * .5;
+
+		var wanderForce =new THREE.Vector3();
+
+		wanderForce = circleCenter.add(displacement);
+
+		this.direction=wanderForce;
+		return wanderForce
+	}
+}
+
+Bullet.prototype = Object.create(THREE.Mesh.prototype);
+
+Bullet.prototype.constructor = Bullet;
+
+Bullet.RADIUS = 10;
+
+Bullet.CIRCLE_DISTANCE=40;
+
+Bullet.CIRCLE_RADIUS=5;
+
+Bullet.steering=new THREE.Vector3(0.0,0.0,0.0); 
+
+
+Bullet.prototype.update = (function() {
+	var scaledDirection = new THREE.Vector3();
+	  return function(delta) {
+	  	if(Bullet.target){
+	  		Bullet.target.clone().sub(this.position).length()<=Bullet.RADIUS?(
+	  				Bullet.target=null,
+	  				scene.remove(target)
+	  			):null;
+	  	}
+	  	var from= this.position;
+	  	this.direction=this.direction.add(this.wander()).normalize();
+
+	 	scaledDirection.copy(this.direction).multiplyScalar(this.speed*delta);
+		this.position.add(scaledDirection)
+		Bullet.target?(
+			this.changeDirection(),
+			this.position.add(Bullet.steering)
+			):null;
+		var to= new THREE.Vector3(this.position.x,this.position.y,this.position.z)
+		this.drawline(from,to);
+	};
+})();
+
+Bullet.prototype.changeDirection=(function(){
+	var steering= new THREE.Vector3(),desired_velocity=new THREE.Vector3();
+	return function(target){
+		target=target||Bullet.target;
+		desired_velocity=target.clone().sub(this.position).normalize().multiplyScalar(10);
+		Bullet.steering=desired_velocity.sub(this.direction).divideScalar(1.9);
+		Bullet.target=target;
+	}
+})();
+
+Bullet.prototype.clone = function(object) {
+	if (typeof object === 'undefined') {
+		object = new Bullet();
+	}
+	THREE.Mesh.prototype.clone.call(this, object);
+
+	object.direction = this.direction;
+	object.speed = this.speed;
+	object.damage = this.damage;
+
+	return object;
+};
---
+++ article/fly/flymain.js
@@ -0,0 +1,333 @@
+// GLOBALS ====================================================================
+
+var scene, renderer, camera, clock, player;
+
+var bufferScene;
+var textureA;
+var textureB;
+var bufferMaterial;
+var bufferObject;
+var finalMaterial;
+var quad;
+var plane
+var target;
+
+var HORIZONTAL_UNIT = 100,
+			VERTICAL_UNIT = 100,
+		INV_MAX_FPS = 1 / 100,
+		FIRING_DELAY = 1000,
+		BOT_MAX_AXIAL_ERROR = 10,
+		BOT_MOVE_DELAY = 2000;
+var frameDelta = 0,
+		paused = true;
+var spawnPoints = [];
+var floor;
+var bullets = [],bullet,
+		deadBulletPool = [];
+var enemies = [],
+		numEnemies = 5;
+
+// MAPS =======================================================================
+
+var map = "   XXXXXXXX     XXXXXXXX      \n" +
+					"   X      X     X      X      \n" +
+					"   X  S   X     X   S  X      \n" +
+					"   X      XXXXXXX      X      \n" +
+					"   X                  XXXXXXXX\n" +
+					"   X         S               X\n" +
+					"   XXXX XX       XXXX    S   X\n" +
+					"      X XX       X  X        X\n" +
+					"   XXXX XXX     XX  X        X\n" +
+					"   X      XX   XXXXXXTTXX  XXX\n" +
+					"   X      XTTTTTXXXTTTTXX  X  \n" +
+					"   XX  S  XTTTTTXXTTTTTXX  XXX\n" +
+					"XXXXX     XTTTTTXTTTTTTX     X\n" +
+					"X      XTTXTTTTTTTTTTTTX     X\n" +
+					"X  S  XXTTTTTTTTXTTTTTXX  S  X\n" +
+					"X     XTTTTTTTTTXTTTTTX      X\n" +
+					"X     TTTTTTTTTTXXXXTTX  XXXXX\n" +
+					"X     XTTTTTTTTTX X      X    \n" +
+					"XX  XXXTTTTTTTTTX X      X    \n" +
+					" X  X XTTTTTTTTTX X      X    \n" +
+					" X  XXX         X X      X    \n" +
+					" X             XXXX      XX   \n" +
+					" XXXXX    T               X   \n" +
+					"     X                 S  X   \n" +
+					"     XX   S  XXXXXXXX     X   \n" +
+					"      XX    XX      XXXXXXX   \n" +
+					"       XXXXXX                 ";
+map = map.split("\n");
+var ZSIZE = map.length * HORIZONTAL_UNIT,
+		XSIZE = map[0].length * HORIZONTAL_UNIT;
+var meshMap = new Array(map.length);
+
+function MapCell() {
+	this.set.apply(this, arguments);
+}
+MapCell.prototype.set = function(row, col, char, mesh) {
+	this.row = row;
+	this.col = col;
+	this.char = char;
+	this.mesh = mesh;
+	return this;
+};
+
+// ANIMATION LOOP =============================================================
+
+function animate() {
+	draw();
+
+	frameDelta += clock.getDelta();
+	while (frameDelta >= INV_MAX_FPS) {
+		update(INV_MAX_FPS);
+	 	bufferMaterial.uniforms.smokeSource.value.z = 0.1;
+        //UpdateMousePosition(window.innerWidth/2,window.innerHeight/2);
+		frameDelta -= INV_MAX_FPS;
+	}
+
+		requestAnimationFrame(animate);
+	
+}
+
+// SETUP ======================================================================
+
+function setup() {
+	setupThreeJS();
+	setupWorld();
+	buffer_texture_setup();
+	requestAnimationFrame(animate);
+}
+
+function setupThreeJS() {
+	scene = new THREE.Scene();
+	//scene.fog = new THREE.FogExp2(0xdcf7e7, 0.001);
+
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0xffffff,1);
+	document.body.appendChild(renderer.domElement);
+
+	camera = new THREE.PerspectiveCamera(60, renderer.domElement.width / renderer.domElement.height, 1, 10000);
+	camera.rotation.x=-Math.PI/6;
+	camera.position.set(0,800,1800);
+	
+	clock = new THREE.Clock(false);
+}
+
+function setupWorld() {
+	setupMap();
+
+	// player = new Player();
+	// player.add(camera);
+	// player.position.y = 20;
+	// scene.add(player);
+
+	 var light = new THREE.DirectionalLight(0xffffff, 2.5);
+	 light.position.set(1, 1, 1);
+	 scene.add(light);
+	 light = new THREE.DirectionalLight(0xf3faff, 0.75);
+	 light.position.set(-1, - 0.5, -1);
+	 //scene.add(light);
+}
+
+function setupMap() {
+	for (var i = 0, rows = map.length; i < rows; i++) {
+		for (var j = 0, cols = map[i].length; j < cols; j++) {
+			if (typeof meshMap[i] === 'undefined') {
+				meshMap[i] = new Array(cols);
+			}
+			meshMap[i][j] = addVoxel(map[i].charAt(j), i, j);
+		}
+	}
+
+	
+}
+
+var addVoxel = (function() {
+	var XOFFSET = map.length * 0.5 * HORIZONTAL_UNIT,
+			ZOFFSET = map[0].length * 0.5 * HORIZONTAL_UNIT,
+			materials = [];
+	for (var i = 0; i < 8; i++) {
+		materials.push(new THREE.MeshPhongMaterial({
+			color: new THREE.Color().setHSL(Math.random() * 0.2 + 0.3, 0.5, Math.random() * 0.25 + 0.75)
+		}));
+	}
+	function material() {
+		return materials[Math.floor(Math.random() * materials.length)].clone();
+	}
+
+	var WALL = new THREE.CubeGeometry(HORIZONTAL_UNIT, VERTICAL_UNIT, HORIZONTAL_UNIT);
+
+	return function(type, row, col) {
+		var z = (row+1) * HORIZONTAL_UNIT - ZOFFSET,
+				x = (col+1) * HORIZONTAL_UNIT - XOFFSET,
+				mesh;
+		switch(type) {
+			case ' ': break;
+			case 'S':
+				spawnPoints.push(new THREE.Vector3(x, 0, z));
+				break;
+			case 'T':
+				mesh = new THREE.Mesh(WALL.clone(), material());
+				mesh.position.set(x, VERTICAL_UNIT*0.5, z);
+				break;
+			case 'X':
+				mesh = new THREE.Mesh(WALL.clone(), material());
+				mesh.scale.y = 3;
+				mesh.position.set(x, VERTICAL_UNIT*1.5, z);
+				break;
+		}
+		if (typeof mesh !== 'undefined') {
+			//scene.add(mesh);
+		}
+		return mesh;
+	};
+})();
+
+var checkBulletCollision = (function() {
+	var cell = new MapCell();
+	function removeBullet(bullet) {
+		scene.remove(bullet);
+	}
+	return function(bullet) {
+		
+		mapCellFromPosition(bullet.position, cell);
+		if (cell.char == 'X' ||
+				(cell.char == 'T' && bullet.position.y - Bullet.RADIUS < cell.mesh.position.y + VERTICAL_UNIT * 0.5) ||
+				bullet.position.y - Bullet.RADIUS < floor.position.y ||
+				bullet.position.y > VERTICAL_UNIT * 5) {
+		    	bullet.direction=new THREE.Vector3(-bullet.direction.x,-bullet.direction.y,-bullet.direction.z)
+		    	//scene.add(cell.mesh);
+		}
+	};
+})();
+
+function mapCellFromPosition(position, cell) {
+	cell = cell || new MapCell();
+	var XOFFSET = (map.length+1) * 0.5 * HORIZONTAL_UNIT,
+			ZOFFSET = (map[0].length+1) * 0.5 * HORIZONTAL_UNIT;
+	var mapCol = Math.floor((position.x + XOFFSET) / HORIZONTAL_UNIT) - 1,
+			mapRow = Math.floor((position.z + ZOFFSET) / HORIZONTAL_UNIT) - 1,
+			char = map[mapRow].charAt(mapCol),
+			mesh = meshMap[mapRow][mapCol];
+	return cell.set(mapRow, mapCol, char, mesh);
+}
+
+// SHOOTING ===================================================================
+
+var shoot = (function() {
+	var error = new THREE.Vector3();
+	function produceError(deg) {
+		if (typeof deg === 'undefined') deg = BOT_MAX_AXIAL_ERROR;
+		return Math.random() * (deg / 90) - (deg / 180);
+	}
+	return function() {
+		var from = new THREE.Vector3(-600, 200, 600);
+		var to=new THREE.Vector3(1600, 200, 600);
+		bullet = new Bullet();
+		bullet.position.copy(from);
+		bullet.rotation.copy(new THREE.Vector3(0, 0, 0));
+		bullet.direction = to.clone().sub(from).normalize();
+		error.set(produceError(), produceError(), produceError());
+		bullet.direction.add(error);
+		scene.add(bullet);
+	};
+})();
+
+function spawn(unit) {
+	unit = unit || player;
+	var cell = new MapCell(), point;
+	// do {
+	// 	point = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
+	// 	mapCellFromPosition(point, cell);
+	// } while (isPlayerInCell(cell.row, cell.col));
+	point = spawnPoints[Math.floor(Math.random() * spawnPoints.length)];
+	unit.position.copy(point);
+	unit.position.y = 200;
+	var direction = (point.z > 0 ? 0 : -1) * Math.PI;
+	unit.rotation.set(0, direction, 0);
+}
+
+// DRAW =======================================================================
+
+function draw() {
+
+	renderer.render(bufferScene,camera,textureB,true);
+	//Swap textureA and B
+	var t = textureA;
+	textureA = textureB;
+	textureB = t;
+	quad.material.map = textureB;
+	bufferMaterial.uniforms.bufferTexture.value = textureA;
+	renderer.render(scene, camera);
+}
+
+// UPDATE =====================================================================
+
+function update(delta) {
+
+	bullet.update(delta);
+	checkBulletCollision(bullet);
+
+}
+
+function buffer_texture_setup(){
+    //Create buffer scene
+    bufferScene = new THREE.Scene();
+    //Create 2 buffer textures
+    textureA = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+    textureB = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter} );
+    //Pass textureA to shader
+    bufferMaterial = new THREE.ShaderMaterial( {
+        uniforms: {
+         bufferTexture: { type: "t", value: textureA },
+         res : {type: 'v2',value:new THREE.Vector2(window.innerWidth,window.innerHeight)},
+         smokeSource:{type:"v3",value:new THREE.Vector3(0,0,0)}
+        },
+        fragmentShader: document.getElementById( 'fragShader' ).innerHTML
+    } );
+    plane = new THREE.PlaneBufferGeometry( XSIZE, ZSIZE );
+    bufferObject = new THREE.Mesh( plane, bufferMaterial );
+    bufferScene.add(bufferObject);
+
+    //Draw textureB to screen 
+    finalMaterial =  new THREE.MeshBasicMaterial({map: textureB});
+    quad = new THREE.Mesh( plane, finalMaterial );
+    quad.position.set(HORIZONTAL_UNIT, 0, -HORIZONTAL_UNIT); // Ideally this wouldn't be needed
+    quad.rotation.x = Math.PI * -0.5;
+   // scene.add(quad);
+    var material = new THREE.MeshPhongMaterial({ color: 0xaaaaaa });
+	var floorGeo = new THREE.PlaneGeometry(XSIZE, ZSIZE, 20, 20);
+	floor = new THREE.Mesh(floorGeo, material);
+	floor.rotation.x = Math.PI * -0.5;
+	floor.position.set(HORIZONTAL_UNIT, 0, -HORIZONTAL_UNIT); // Ideally this wouldn't be needed
+	scene.add(floor);
+}
+
+
+
+
+
+document.addEventListener('click', function(event) {
+	target?scene.remove(target):null;
+
+	target = new Bullet();
+
+	spawn(target);
+
+	bullet.changeDirection(target.position);
+
+	scene.add(target);
+	
+});
+
+setup();
+
+shoot();
+
+
+clock.start();
+
+
+
+
